aa03a2864b3cdf7a46d92e24e306ada36e43af87
Brennen Green <bgreen@Techs-iMac-4.local>
2019-06-11 12:00:56 -0400

    Practice

--- practice.py
+++ practice.py
@@ -8,64 +8,66 @@ EOF        = 'EOF'
 class Token(object):
     def __init__(self, type, value):
         self.type = type
-        self.value = repr(value)
+        self.value = value
     
     def __str__(self):
-        return "Token({},{})".format(
-            self.type,
-            self.value
+        return "Token({type},{value})".format(
+            type = self.type,
+            value = repr(self.value)
         )
     
     def __repr__(self):
         return self.__str__()
 
 class Interpreter(object):
-    def __init__(self, stream):
-        self.stream = stream
+    def __init__(self, text):
+        self.text = text
         self.pos   = 0
-        self.current_character = self.stream[0]
         self.current_token     = None
+        self.current_character = self.text[self.pos]
     
     def error(self):
         raise Exception("Syntax Error")
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_token = None
+        else:
+            self.current_character = self.text[self.pos]
 
     def skip_whitespace(self):
         while self.current_character is not None and self.current_character.isspace():
             self.advance()
 
+    def integer(self):
+        result = ''
+        while self.current_character is not None and self.current_character.isdigit():
+            result += self.current_character
+            self.advance
+        return int(result)
+
+
     def get_next_token(self):
         while self.current_character is not None:
-            if self.current_character.isdigit():v 
+            if self.current_character.isdigit(): 
                 return Token(INTEGER, self.integer())
             
             if self.current_character.isspace():
                 self.skip_whitespace()
                 continue
 
-            if self.current_token in (PLUS, MINUS):
-                if self.current_token == PLUS:
-                    self.advance()
-                    return Token(PLUS, '+')
-                if self.current_token == MINUS:
-                    self.advance()
-                    return Token(MINUS, '-')
-            
+            if self.current_character == '+':
+                self.advance()
+                return Token(PLUS, '+')
+            if self.current_character == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
             self.error()
+
         return Token(EOF, None)
 
-    def advance(self):
-        self.pos += 1
-        if self.pos > len(self.stream) - 1:
-            self.current_token = None
-        else:
-            self.current_character = self.stream[self.pos]
-
-    def integer(self):
-        result = ''
-        while self.current_character.isdigit():
-            result += self.current_character
-            self.advance
-        return int(result)
 
     def eat(self, token_type):
         if self.current_token.type == token_type:
@@ -73,6 +75,36 @@ class Interpreter(object):
         else:
             self.error()
 
+    def term(self):
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
     def expr(self):
         self.current_token = self.get_next_token()
+
+        result = self.term()
+        while self.current_token in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result = result - self.term()
+
+        return result
     
+def main():
+	while True:
+		try:
+			text = input("calc> ")
+		except EOFError:
+			break
+		if not text:
+			continue
+		
+		print(Interpreter(text).expr())
+
+if __name__ == "__main__":
+	main()
